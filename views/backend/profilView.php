93d3562aec47f364337160d1885b2e8d92db78a1
PaulineMartin <paumichel32@gmail.com>
2019-09-20 22:22:50 +0200

    Create managing articles

--- views/backend/profilView.php
+++ views/backend/profilView.php
@@ -6,12 +6,12 @@
 
 <nav class="nav_icons_container">
     <div class="icons_container">
-        <a class="go_home icons" href="index.php?action=admin"><img alt="icon_go_home" src="public/images/home.png"></a>
-        <a class="add_article icons" href="index.php?action=admin_article"><img alt="icon_add_article" src="public/images/article.png"></a>
-        <a class="add_portrait icons" href="index.php?action=admin_portrait"><img alt="icon_add_portait" src="public/images/portrait.png"></a>
-        <a class="add_projet icons" href="index.php?action=admin_projet"><img alt="icon_add_projet" src="public/images/projet.png"></a>
-        <a class="manage_comments icons" href="index.php?action=admin_comments"><img alt="icon_comments" src="public/images/commentaires.png"></a>
-        <a class="manage_messages icons" href="index.php?action=admin_messages"><img alt="icon_messages" src="public/images/messagerie.png"></a>
-        <a class="arrow icons" href="index.php?action=home"><img alt="icon_arrow" src="public/images/retour-fleche.png"></a>
+        <a class="go_home icones_menu" href="index.php?action=admin"><img alt="icon_go_home" src="public/images/home.png"></a>
+        <a class="add_portrait icones_menu" href="index.php?action=admin_portrait"><img alt="icon_add_portait" src="public/images/portrait.png"></a>
+        <a class="add_projet icones_menu" href="index.php?action=admin_projet"><img alt="icon_add_projet" src="public/images/projet.png"></a>
+        <a class="manage_comments icones_menu" href="index.php?action=admin_comments"><img alt="icon_comments" src="public/images/commentaires.png"></a>
+        <a class="manage_messages icones_menu" href="index.php?action=admin_messages"><img alt="icon_messages" src="public/images/messagerie.png"></a>
+        <a class="profil icones_menu" href="index.php?action=admin_profil"><img alt="icon_manage_profil" src="public/images/user.png"></a>
+        <a class="arrow icones_menu" href="index.php?action=home"><img alt="icon_arrow" src="public/images/retour-fleche.png"></a>
     </div>
 </nav>
