36988b6459b937e6cf203d9bc7aa9940a41fdaaf
PaulineMartin <paumichel32@gmail.com>
2019-12-15 17:26:22 +0100

    Change bdd connect

--- models/Manager.php
+++ models/Manager.php
@@ -10,7 +10,7 @@ abstract class Manager
     {
         try {
             $db = new PDO(
-                'mysql:host=localhost;dbname=projet_5;charset=utf8', 'root', "root",
+                'mysql:host=localhost;dbname=mapa8726_projet_5;charset=utf8', 'mapa8726_paulineweb', "Gaspard0415",
                 array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
             );
         return $db;
